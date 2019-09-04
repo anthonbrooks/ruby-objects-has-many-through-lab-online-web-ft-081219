3945043ed40251e0214db5ffd8d443cac246f191
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-04 20:14:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,14 +12,14 @@ class Artist
   def self.all
     @@all 
   end 
-  def new_song 
-    
+  
+  def new_song(name, genre)
+    Song.new(name, genre)
   end 
+  
   def songs 
     Song.all.select { |song| song.artist == self }
   end 
-  
-  
   
   def genres 
     
