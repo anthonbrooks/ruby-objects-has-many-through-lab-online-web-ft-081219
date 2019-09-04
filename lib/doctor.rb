7ff38500f67dc12a14c1ceefddec46b28f70e36d
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-04 20:09:55 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,24 @@
+class Doctor
+  
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  def apointments 
+    
+  end 
+  
+  def new_appointment
+    
+  end 
+  
+  def patients
+    
+  end 
+  
+end 
