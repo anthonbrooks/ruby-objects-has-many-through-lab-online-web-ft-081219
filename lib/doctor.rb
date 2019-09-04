50f07bea34c1be17797d756e250ebec6deaf8a12
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-04 20:56:41 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,26 +1,28 @@
 class Doctor
-  attr_accessor :name
-
+  
+  attr_accessor :name 
+  
   @@all = []
-
+  
   def initialize(name)
     @name = name
     @@all << self
-  end
-
+  end 
+  
   def self.all
-    @@all
-  end
-
-  def new_appointment(patient, date)
-    Appointment.new(date, patient, self)
-  end
-
-  def appointments
+    @@all 
+  end 
+  
+  def appointments 
     Appointment.all.select { |appointment| appointment.doctor == self }
-  end
-
+  end 
+  
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end 
+  
   def patients
     appointments.map(&:patient)
-  end
-end
+  end 
+  
+end 
